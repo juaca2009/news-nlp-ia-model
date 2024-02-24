6b35fac5331306350dee5001a9a2fa4561680d11
juaca2009 <juaca2009@gmail.com>
2024-02-24 11:47:13 -0500

    configuracion proyecto

---
+++ entorno.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+iniciar_entorno(){
+
+    cd ~/.virtualenvs/news-npl-ia/bin
+    source ./activate
+    cd ../../../Documentos/'workPlace python'/news-nlp-ia-model
+}
+
+iniciar_entorno
