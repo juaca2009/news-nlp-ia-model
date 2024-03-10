7e382bc8dd38747f3248c6c442b04b89d1d29d36
juaca2009 <juaca2009@gmail.com>
2024-03-10 13:22:03 -0500

    implementacion modelos

--- src/model/create_representation.py
+++ src/model/create_representation.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from gensim.models import Word2Vec
 
 NAME_FILE = 'noticias.csv'
 
@@ -9,10 +12,30 @@ def load_data():
         print(f"Error: No se encontró el archivo '{NAME_FILE}'. La ejecución se detendrá.")
         raise SystemExits
     
+def bag_of_words(words):
+    strings = [' '.join(tokens) for tokens in words]
+    vectorizer = CountVectorizer()
+    matriz_bow = vectorizer.fit_transform(strings)
+    return pd.DataFrame(matriz_bow.toarray(), columns=vectorizer.get_feature_names_out())
+
+def tf_idf(words):
+    strings = [' '.join(tokens) for tokens in words]
+    tfidf_vectorizer = TfidfVectorizer()
+    tf = tfidf_vectorizer.fit_transform(strings)
+    return pd.DataFrame(tf.toarray(), columns=tfidf_vectorizer.get_feature_names_out())
+
+def word_2_vec(words):
+    return Word2Vec(sentences=words, vector_size=100, window=5, min_count=1, workers=4)
+
+    
 if __name__ == "__main__":
     df = load_data()
     df['title'] = df['title'].apply(lambda x: x.split(' '))
-
     #para obtener lista con listas de strings
-    #columna_title_lista = df['title'].tolist() 
+    columna_title_lista = df['title'].tolist() 
+    filtrado = [titulo_lista for tipo, titulo_lista in zip(df['type_new'], columna_title_lista) if tipo == 'Politica']
+
+    df = tf_idf(columna_title_lista)
+    print(df)
+    #df.to_csv('prueba.csv', index=False)
     
